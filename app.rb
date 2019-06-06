6c8e1eb7bd9482439683ff118dd21a2b8b7b15e2
Ethan Sagin <ethansagin@gmail.com>
2019-06-06 14:42:28 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,10 +6,17 @@ class App < Sinatra::Base
     erb :user_input
   end
   
+<<<<<<< HEAD
   post '/piglatinize' do
     @phrase = params[:user_phrase]
     
     erb :piglatinize
+=======
+  post 'piglatinize'
+    # @phrase = params[:user_phrase]
+    
+    # erb :piglatinize
+>>>>>>> 83d59ead51ffb83fc477eb7adf855ea19860672d
   end
 
 end
